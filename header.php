d09b933cfbcb2b6819dfe90805baf42871e02ff2
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2024-02-19 17:18:47 +0800

    - Just formatting the header icon from <i> ->  <svg> except logout icon

--- header.php
+++ header.php
@@ -25,6 +25,7 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
     <script src="scripts\navbar_script.js" defer></script>
+    <script src="scripts\live-script.js" defer></script>
 </head>
 
 <body>
@@ -46,7 +47,7 @@
                         <li class="nav__item">
                             <a class="nav__link" href='index.php'>
                                 <i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        style="fill: #ffffffbf;transform: ;msFilter:;">
+                                        style="fill: #ffffffbf;transform: ;msFilter:;".>
                                         <path
                                             d="M12.71 2.29a1 1 0 0 0-1.42 0l-9 9a1 1 0 0 0 0 1.42A1 1 0 0 0 3 13h1v7a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7h1a1 1 0 0 0 1-1 1 1 0 0 0-.29-.71zM6 20v-9.59l6-6 6 6V20z">
                                         </path>

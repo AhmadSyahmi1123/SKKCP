1b583e734a8f67155a8a75ecb392a787ef2d27ef
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2024-06-07 20:56:01 +0800

    feat: add toast after updating user info

--- ahli-kemaskini-proses.php
+++ ahli-kemaskini-proses.php
@@ -3,43 +3,48 @@
 session_start();
 
 # Memanggil fail kawalan-admin.php
-include("kawalan-admin.php");
+include ("kawalan-admin.php");
 
 # Semak kewujudan data POST
-if(!empty($_POST)){
+if (!empty($_POST)) {
     # Memanggil fail connection.php
-    include("connection.php");
+    include ("connection.php");
 
     # Pengesahan data nokp ahli
-    if(strlen($_POST["nokp"]) != 12 or !is_numeric($_POST["nokp"])){
+    if (strlen($_POST["nokp"]) != 12 or !is_numeric($_POST["nokp"])) {
         die("<script>alert('Ralat No Kad Pengenalan');
         window.history.back();</script>");
     }
 
     # Arahan SQL (query) untuk kemaskini maklumat ahli
     $arahan = "update ahli set
-    nama = '".strtoupper($_POST['nama'])."',
-    nokp = '".$_POST['nokp']."',
-    katalaluan = '".$_POST['katalaluan']."',
-    IDkelas = '".$_POST['IDkelas']."',
-    tahap = '".$_POST['tahap']."'
+    nama = '" . strtoupper($_POST['nama']) . "',
+    nokp = '" . $_POST['nokp'] . "',
+    katalaluan = '" . $_POST['katalaluan'] . "',
+    IDkelas = '" . $_POST['IDkelas'] . "',
+    tahap = '" . $_POST['tahap'] . "'
     where
-    nokp = '".$_GET['nokp_lama']."'
+    nokp = '" . $_GET['nokp_lama'] . "'
     ";
 
     # Laksana dan semak proses kemaskini
-    if(mysqli_query($condb, $arahan)){
+    if (mysqli_query($condb, $arahan)) {
         # Kemaskini berjaya
-        echo "<script>alert('Kemaskini Berjaya!');
-        window.location.href='senarai-ahli.php';</script>";
-    }
-    else{
+        # Kemaskini berjaya
+        $message = "Maklumat Ahli Berjaya Dikemaskini!";
+        $notificationType = 'success';
+        $notificationMessage = $message;
+    } else {
         # Kemaskini gagal
-        echo "<script>alert('Kemaskini Gagal');
-        window.history.back();</script>";
+        $message = "Ralat! Maklumat Ahli Gagal Dikemaskini!";
+        $notificationType = 'error';
+        $notificationMessage = $message;
     }
-}
-else{
+
+    // Redirect with notification parameters
+    header("Location: senarai-aktiviti.php?notificationType=$notificationType&notificationMessage=$notificationMessage");
+    exit();
+} else {
     # Jika data GET tidak wujud, kembali ke fail senarai-ahli.php
     die("<script>alert('Sila lengkapkan data');
     window.location.href='senarai-ahli.php';</script>");

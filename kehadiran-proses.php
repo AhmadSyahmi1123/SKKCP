6f7c7e0e8bafb130554f0e633c1e13164bec1abc
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2024-06-07 21:02:07 +0800

    fix: point logic where it'll still give user point even though they already sign their attendance

--- kehadiran-proses.php
+++ kehadiran-proses.php
@@ -2,20 +2,27 @@
 # Memanggil fail connection.php
 include ("connection.php");
 
+$IDaktiviti = $_GET['IDaktiviti'];
+
+# Dapatkan senarai nokp yang sudah hadir sebelum ini
+$existing_nokp = [];
+$result_existing = mysqli_query($condb, "SELECT nokp FROM kehadiran WHERE IDaktiviti='$IDaktiviti'");
+while ($row = mysqli_fetch_assoc($result_existing)) {
+    $existing_nokp[] = $row['nokp'];
+}
+
 # Memadam data kehadiran lama supaya data kehadiran baru dapat dimasukkan
-$sql_padam = mysqli_query($condb, "delete from kehadiran where IDaktiviti='" . $_GET['IDaktiviti'] . "'");
+$sql_padam = mysqli_query($condb, "DELETE FROM kehadiran WHERE IDaktiviti='$IDaktiviti'");
 
 $masa = date("H:i:s");
 foreach ($_POST['kehadiran'] as $nokp) {
     # Menyimpan semula data kehadiran baru
-    $simpan_data = mysqli_query($condb, "insert into kehadiran
-    (nokp, IDaktiviti, masa_hadir) values
-    ('$nokp', '" . $_GET['IDaktiviti'] . "', '$masa') ");
+    $simpan_data = mysqli_query($condb, "INSERT INTO kehadiran (nokp, IDaktiviti, masa_hadir) VALUES ('$nokp', '$IDaktiviti', '$masa')");
 
-    $kemaskini_mata = mysqli_query($condb, "update ahli
-    set mata = mata + 100
-    where nokp = '$nokp'
-    ");
+    # Kemaskini mata hanya jika pengguna tidak mempunyai rekod kehadiran sebelumnya
+    if (!in_array($nokp, $existing_nokp)) {
+        $kemaskini_mata = mysqli_query($condb, "UPDATE ahli SET mata = mata + 100 WHERE nokp = '$nokp'");
+    }
 }
 
 # Laksana dan semak proses kemaskini
@@ -26,7 +33,7 @@ if ($simpan_data) {
     $notificationMessage = $message;
 } else {
     # Kemaskini gagal
-    $message = "Ralat! Kemaskini Gagal Dikemaskini!";
+    $message = "Ralat! Kehadiran Gagal Dikemaskini!";
     $notificationType = 'error';
     $notificationMessage = $message;
 }
